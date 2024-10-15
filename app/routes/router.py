45f1a9c0a7bd7dc581246dc22a3a7bb8f1303d45
RodrigoFA216 <rfloresa1900@alumno.ipn.mx>
2024-10-15 02:25:15 -0600

    modify router, add media response and remove at least as a background task

--- app/routes/router.py
+++ app/routes/router.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, BackgroundTasks
 from fastapi.responses import JSONResponse, FileResponse
 import os
+import shutil
 
 #from app.schemas.item_scheme import ItemScheme
 
@@ -34,22 +35,22 @@ async def reciveImage(file: UploadFile = File(...)):
         )
 
 @router.post("/API/Get/Video/Detection", tags=["Get", "Video"])
-async def reciveImage(file: UploadFile = File(...)):
+async def reciveImage(background_tasks: BackgroundTasks, file: UploadFile = File(...), ):
     try:
         videos_dir = os.path.join(os.path.dirname(__file__), "../temp/videos")
-        
         os.makedirs(videos_dir, exist_ok=True)
         
         if file.filename[-4:] in vidFormats:
             file_path = os.path.join(videos_dir, file.filename)
             
+            # Guardar el archivo en el directorio
             with open(file_path, 'wb') as video_file:
                 video_file.write(await file.read())
             
-            return JSONResponse(
-                content={"message": "Reciving data", "file": file.filename},
-                status_code=202
-            )
+            # Agregar tarea en segundo plano para eliminar el archivo después de la respuesta
+            background_tasks.add_task(os.remove, file_path)
+            
+            return FileResponse(file_path, filename=file.filename, media_type=file.content_type)
         else:
             return JSONResponse(
                 content={"Error": "La extensión del archivo no es válida"},
